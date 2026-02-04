eec8bb895731dddb4c46a1c3fa9085b336f2ba28
Aditya <realraghavaditya@gmail.com>
2026-02-04 16:45:29 +0530

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,11 +1,11 @@
 export const siteConfig = {
-  name: 'Encephalon Lab',
+  name: 'Lexa',
   url: 'https://encephalon-lab.vercel.app',
   // ogImage: "https://ui.shadcn.com/og.jpg",
   description:
-    'With Encephalon Lab, harness the power of AI agents to supercharge your productivity in a snap',
+    'With Lexa, harness the power of AI agents to supercharge your productivity in a snap',
   links: {
-    github: 'https://github.com/raghav-45/encephalon-lab',
+    github: 'https://github.com/raghav-45/lexa-ai',
   },
   keywords: [
     'React',
